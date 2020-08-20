4d53c9b79a01b52627750810b096d4e7183a2ef7
Pablo Tobalina <pablotobalina@gmail.com>
2020-08-20 20:15:25 +0200

    Show projects full paths

--- SolutionProjectsDependencies.ps1
+++ SolutionProjectsDependencies.ps1
@@ -1,11 +1,13 @@
 $solutionFilePath = Read-Host 'Full path'
 
+$solutionFolderPath = Split-Path $solutionFilePath
+
 Get-Content $solutionFilePath |
 Select-String 'Project\(' |
 ForEach-Object {
     $projectParts = $_ -Split '[,=]' | ForEach-Object { $_.Trim('[ "{}]') };
     New-Object PSObject -Property @{
         Name = $projectParts[1];
-        Path = $projectParts[2];
+        Path = $solutionFolderPath + '\' + $projectParts[2];
     }
 }
