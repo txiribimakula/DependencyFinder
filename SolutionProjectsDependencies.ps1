625c96d06b1e8583cf1f2c444a4400f42a24bbac
Pablo Tobalina <pablotobalina@gmail.com>
2020-08-20 20:47:43 +0200

    Apply same code from the project dependency reader

--- SolutionProjectsDependencies.ps1
+++ SolutionProjectsDependencies.ps1
@@ -1,3 +1,6 @@
+
+
+
 $solutionFilePath = Read-Host 'Full path'
 
 $solutionFolderPath = Split-Path $solutionFilePath
@@ -6,8 +9,17 @@ Get-Content $solutionFilePath |
 Select-String 'Project\(' |
 ForEach-Object {
     $projectParts = $_ -Split '[,=]' | ForEach-Object { $_.Trim('[ "{}]') };
-    New-Object PSObject -Property @{
-        Name = $projectParts[1];
-        Path = $solutionFolderPath + '\' + $projectParts[2];
+    Write-Host $projectParts[1]
+
+    $projectFilePath = $solutionFolderPath + '\' + $projectParts[2]
+    [xml]$projectXml = Get-Content $projectFilePath
+
+    $ns = new-object Xml.XmlNamespaceManager $projectXml.NameTable
+    $ns.AddNamespace("msb", "http://schemas.microsoft.com/developer/msbuild/2003")
+
+    $projectReferenceNodes = $projectXml.SelectNodes('//msb:ProjectReference', $ns)
+
+    foreach ($projectReference in $projectReferenceNodes) {
+        Write-Host $projectReference.Include
     }
 }
