b6b1a3da81d677279499ca64f530dba74693ffd1
Pablo Tobalina <pablotobalina@gmail.com>
2020-08-20 20:56:50 +0200

    Extract code into function

--- SolutionProjectsDependencies.ps1
+++ SolutionProjectsDependencies.ps1
@@ -1,3 +1,20 @@
+function ProjectDependencies {
+    param (
+        $projectFilePath
+    )
+    
+    [xml]$projectXml = Get-Content $projectFilePath
+
+    $ns = new-object Xml.XmlNamespaceManager $projectXml.NameTable
+    $ns.AddNamespace("msb", "http://schemas.microsoft.com/developer/msbuild/2003")
+
+    $projectReferenceNodes = $projectXml.SelectNodes('//msb:ProjectReference', $ns)
+
+    foreach ($projectReference in $projectReferenceNodes) {
+        Write-Host $projectReference.Include
+    }
+}
+
 $solutionFilePath = Read-Host 'Full path'
 
 $solutionFolderPath = Split-Path $solutionFilePath
@@ -9,14 +26,6 @@ ForEach-Object {
     Write-Host $projectParts[1]
 
     $projectFilePath = $solutionFolderPath + '\' + $projectParts[2]
-    [xml]$projectXml = Get-Content $projectFilePath
-
-    $ns = new-object Xml.XmlNamespaceManager $projectXml.NameTable
-    $ns.AddNamespace("msb", "http://schemas.microsoft.com/developer/msbuild/2003")
-
-    $projectReferenceNodes = $projectXml.SelectNodes('//msb:ProjectReference', $ns)
-
-    foreach ($projectReference in $projectReferenceNodes) {
-        Write-Host $projectReference.Include
-    }
+    
+    ProjectDependencies $projectFilePath
 }
